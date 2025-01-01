eb3acc15cbe18aee98a7da6f5b582b23e5c7e9a5
Shazzad501 <assazzad501@gmail.com>
2025-01-01 10:15:59 +0600

    firebase configuration done

---
+++ src/Pages/login/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// apiKey: "AIzaSyBU4ndwUVQTsG39a-gCWmoDCWLy3ZD_oNM",
+// authDomain: "food-master-ae61a.firebaseapp.com",
+// projectId: "food-master-ae61a",
+// storageBucket: "food-master-ae61a.firebasestorage.app",
+// messagingSenderId: "537125693509",
+// appId: "1:537125693509:web:acf7905dfb947e0a3b1b2c"
+const Login = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Login;
